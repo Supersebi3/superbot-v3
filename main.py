629d345401c7e6eb6a09c52d6400570143ab3b45
sebi <gdsupersebi3@gmail.com>
2018-08-11 17:18:10 +0200

    Removed something.

--- main.py
+++ main.py
@@ -5,8 +5,4 @@ import libneko
 
 bot = libneko.Bot(command_prefix="s#")
 
-@bot.listen()
-async def on_start():
-    await bot.get_user(242887101018931200).send("I'm ready!")
-
 bot.run(os.environ["TOKEN"])
