6a2df936d9ca08c4723927b8589cad5b4824e12d
sebi <gdsupersebi3@gmail.com>
2018-08-19 19:07:04 +0200

    Added superuser commands.

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
+
 import discord
 from discord.ext import commands
 import libneko
 
 bot = libneko.Bot(command_prefix="s#")
 
+bot.load_extension('libneko.extras.superuser')
 bot.run(os.environ["TOKEN"])
