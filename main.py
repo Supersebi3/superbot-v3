a3cf7ff920209674ca22712efd000a0602097d7f
sebi <gdsupersebi3@gmail.com>
2018-08-19 19:48:33 +0200

    Actually added superuser commands.

--- main.py
+++ main.py
@@ -6,5 +6,12 @@ import libneko
 
 bot = libneko.Bot(command_prefix="s#")
 
+@bot.listen('on_ready')
+@bot.listen('on_connect')
+async def get_owner():
+    app_info = await bot.application_info()
+    bot.app_info = app_info
+    bot.owner_id = app_info.owner.id
+
 bot.load_extension('libneko.extras.superuser')
 bot.run(os.environ["TOKEN"])
