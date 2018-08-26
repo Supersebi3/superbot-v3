f222b82b98c5bc183a9349128a176cbc30a8d4f9
sebi <gdsupersebi3@gmail.com>
2018-08-26 16:27:55 +0200

    Moved some methods.

--- bot.py
+++ bot.py
@@ -17,6 +17,13 @@ class Bot(libneko.Bot):
                 self.strings[name[:2]] = json.load(f)
 
 
+    def getstr(self, user, strname):
+        uconf = self.uconf.get(user.id)
+        lang = uconf['language'] if uconf else 'en'
+        text = self.strings[lang][strname]
+        return text
+
+
     async def process_commands(self, message):
     ctx = await self.get_context(message, cls=Context)
     if ctx.command is None:
--- context.py
+++ context.py
@@ -1,12 +1,7 @@
 from discord.ext import commands
 
 class Context(commands.Context):
-    def getstr(self, strname):
-        uconf = self.bot.uconf.get(self.author.id)
-        lang = uconf['language'] if uconf else 'en'
-        text = self.bot.strings[lang][strname]
-        return text
 
     async def sendstr(self, strname, delete_after=None, *args, **kwargs):
-        return await self.send(self.getstr(strname).format(*args, **kwargs),
+        return await self.send(self.bot.getstr(self.author, strname).format(*args, **kwargs),
             delete_after=delete_after)
