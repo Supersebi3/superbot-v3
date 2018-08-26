a88f55a7a21d2f2a1fb6c2184a247a22882b211d
sebi <gdsupersebi3@gmail.com>
2018-08-26 16:46:31 +0200

    libneko stuff

--- main.py
+++ main.py
@@ -8,11 +8,5 @@ from bot import Bot
 
 bot = Bot(command_prefix="s#")
 
-@bot.listen()
-async def on_connect():
-    app_info = await bot.application_info()
-    bot.app_info = app_info
-    bot.owner_id = app_info.owner_id
-
 bot.load_extension("libneko.extras.superuser")
 bot.run(os.environ["TOKEN"])
