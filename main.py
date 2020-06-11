82dbb35f3941b6dcd248e0ebb69c9eafea8165fe
supersebi3 <gdsupersebi3@gmail.com>
2020-06-11 19:46:34 +0200

    foo

--- main.py
+++ main.py
@@ -15,6 +15,5 @@ async def test(ctx):
     await ctx.send(f"Hi! Python version: ```{sys.version}```dpy version: {discord.__version__}\nlibneko version: {libneko.__version__}")
 
 bot.load_extension("libneko.extras.superuser")
-bot.load_extension("Puzzle")
 
 bot.run(os.environ["TOKEN"])
