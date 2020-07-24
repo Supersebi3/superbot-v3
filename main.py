3a75c677946913a51a8ee394abf899d418907197
supersebi3 <gdsupersebi3@gmail.com>
2020-07-25 00:12:27 +0200

    foo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import logging
 
 import discord
 from discord.ext import commands
@@ -7,12 +8,18 @@ import libneko
 
 from bot import Bot
 
+logging.basicConfig(level=logging.INFO)
+
 bot = Bot(command_prefix="s#", enable_default_help=False)
 
 
 @bot.command()
 async def test(ctx):
     await ctx.send(f"Hi! Python version: ```{sys.version}```dpy version: {discord.__version__}\nlibneko version: {libneko.__version__}")
+
+@bot.listen()
+async def on_ready():
+    await bot.change_presence(activity=discord.Game("testing testing testing"))
 
 bot.load_extension("libneko.extras.superuser")
 bot.load_extension("libneko.extras.help")
