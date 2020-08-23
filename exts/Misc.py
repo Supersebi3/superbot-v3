83269fce0a82aeee75b322697a7ae9e4afd6d14c
supersebi3 <gdsupersebi3@gmail.com>
2020-08-23 22:58:21 +0200

    foo

--- exts/Misc.py
+++ exts/Misc.py
@@ -23,7 +23,7 @@ class Misc(commands.Cog):
             seed = user.id * now.month * now.year
         random.seed(seed)
         gayness = random.random()
-        if isinstance(user, str) and user.casefold() == "gay":
+        if user == "gay":
             gayness = 1
         if you:
             await ctx.send(f"You are {gayness:.1%} gay.")
