2748149674651e0d99fa028fb2fb859af4af934a
supersebi3 <gdsupersebi3@gmail.com>
2020-08-23 22:36:09 +0200

    foo

--- exts/Misc.py
+++ exts/Misc.py
@@ -1,4 +1,6 @@
 import random
+from math import prod
+from typing import Union
 from io import BytesIO
 from datetime import datetime
 
@@ -10,17 +12,22 @@ from pilutils.parse import parse
 
 class Misc(commands.Cog):
     @commands.command()
-    async def howgay(self, ctx, *, user: discord.User = None):
+    async def howgay(self, ctx, *, user: Union[discord.User, str] = None):
         you = user is None
-        if user is None:
+        if you:
             user = ctx.author
         now = datetime.utcnow()
-        random.seed(user.id * now.month * now.year)
+        if isinstance(user, str):
+            seed = prod(ord(c) for c in user) * now.month * now.year
+        else:
+            seed = user.id * now.month * now.year
+        random.seed(seed)
         gayness = random.random()
         if you:
             await ctx.send(f"You are {gayness:.1%} gay.")
         else:
             await ctx.send(f"{user.name} is {gayness:.1%} gay.")
+        random.seed()
 
 
 def setup(bot):
