51c2b46c4d7c2026e58878e075a1de982a3458f2
supersebi3 <gdsupersebi3@gmail.com>
2020-09-11 18:08:53 +0200

    foo

--- exts/Misc.py
+++ exts/Misc.py
@@ -1,8 +1,10 @@
 import random
 from math import prod
 from typing import Union
-from io import BytesIO
+from io import BytesIO, StringIO
 from datetime import datetime
+import inspect
+import textwrap
 
 import discord
 from discord.ext import commands
@@ -18,17 +20,35 @@ class Misc(commands.Cog):
             user = ctx.author
         now = datetime.utcnow()
         if isinstance(user, str):
-            seed = prod(ord(c) for c in user) * now.month * now.year
+            seed = prod(ord(c)+i for i, c in enumerate(user)) * now.month * now.year
         else:
             seed = user.id * now.month * now.year
         random.seed(seed)
         gayness = random.random()
+        if user == "gay":
+            gayness = 1
         if you:
             await ctx.send(f"You are {gayness:.1%} gay.")
         else:
             name = user if isinstance(user, str) else user.name
             await ctx.send(f"{name} is {gayness:.1%} gay.")
         random.seed()
+
+
+    @commands.command()
+    async def source(self, ctx, *, cmd):
+        try:
+            code = textwrap.dedent(inspect.getsource(ctx.bot.get_command(cmd).callback))
+        except AttributeError:
+            return await ctx.send(f"Command \"{cmd}\" not found.")
+        fcode = f"```py\n{code}\n```".replace('`', '\xb4')
+        if len(fcode) > 2000:
+            sio = StringIO()
+            sio.write(code)
+            sio.seek(0)
+            await ctx.send(file=discord.File(sio, f"{cmd}.py"))
+        else:
+            await ctx.send(fcode)
 
 
 def setup(bot):
