8bd5f32df0e5434891cbc7d8f6ed2c6c9a172e4f
supersebi3 <gdsupersebi3@gmail.com>
2021-08-20 19:57:22 +0200

    foo

--- utils/pilcolorblind.py
+++ utils/pilcolorblind.py
@@ -1,42 +1,32 @@
+import numpy as np
 from PIL import Image
 
-T = [
+T = np.array([
     [0.31399022, 0.63951294, 0.04649755],
     [0.15537241, 0.75789446, 0.08670142],
     [0.01775239, 0.10944209, 0.87256922],
-]
+])
 
-invT = [
+invT = np.array([
     [5.47221206, -4.6419601, 0.16963708],
     [-1.1252419, 2.29317094, -0.1678952],
     [0.02980165, -0.19318073, 1.16364789],
-]
+])
 
 Ss = {
-    "normal": [[1, 0, 0], [0, 1, 0], [0, 0, 1]],
-    "protanopia": [[0, 1.05118294, -0.05116099], [0, 1, 0], [0, 0, 1],],
-    "deuteranopia": [[1, 0, 0], [0.9513092, 0, 0.04866992], [0, 0, 1],],
-    "tritanopia": [[1, 0, 0], [0, 1, 0], [-0.86744736, 1.86727089, 0],],
+    "normal": np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]]),
+    "protanopia": np.array([[0, 1.05118294, -0.05116099], [0, 1, 0], [0, 0, 1],]),
+    "deuteranopia": np.array([[1, 0, 0], [0.9513092, 0, 0.04866992], [0, 0, 1],]),
+    "tritanopia": np.array([[1, 0, 0], [0, 1, 0], [-0.86744736, 1.86727089, 0],]),
 }
-
-
-def matmul(matrix, vector):
-    vx, vy, vz = vector
-    Mx, My, Mz = matrix
-    x = vx * Mx[0] + vy * Mx[1] + vz * Mx[2]
-    y = vx * My[0] + vy * My[1] + vz * My[2]
-    z = vx * Mz[0] + vy * Mz[1] + vz * Mz[2]
-    return x, y, z
 
 
 def apply(color, filter, rgba=False):
     if rgba:
         r, g, b, a = color
         color = r, g, b
-    lms = matmul(T, color)
-    lms_filtered = matmul(filter, lms)
-    rgb_filtered = matmul(invT, lms_filtered)
-    r, g, b = rgb_filtered
+    filtered = invT @ filter @ T @ color
+    r, g, b = filtered
     if rgba:
         return round(r), round(g), round(b), a
     return round(r), round(g), round(b)
