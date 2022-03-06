6cd36458dee3c190af2faa92a5575a0247e75f0d
supersebi3 <gdsupersebi3@gmail.com>
2022-03-06 18:28:16 +0100

    foo

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 
 from bot import Bot
 
-__version__ = '0.0.1-p77'
+__version__ = '0.0.1-p78'
 
 # install pilutils after new setuptools has been built
 os.system("pip install git+https://github.com/Supersebi3/pilutils")
@@ -16,7 +16,7 @@ os.system("pip install git+https://github.com/Supersebi3/pilutils")
 logging.basicConfig(level=logging.INFO)
 
 intents = discord.Intents.default()
-intents.message_content = True
+#intents.message_content = True
 
 bot = Bot(command_prefix="s#", intents=intents)
 
